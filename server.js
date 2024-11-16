831bc8c5f93b44102f537e69dd9bdace70cc5064
anto-kazungu <kazoga.antony@gmail.com>
2024-11-16 19:19:13 +0300

    Added visits, edvisits & discharge logics & made code readable

--- server.js
+++ server.js
@@ -73,7 +73,10 @@ db.connect((err) => {
         const query = `
             SELECT 
                 (SELECT COUNT(*) FROM patients) AS totalPatients,
-                (SELECT COUNT(*) FROM providers) AS totalProviders;
+                (SELECT COUNT(*) FROM providers) AS totalProviders,
+                (SELECT COUNT(*) FROM visits) AS totalVisits,
+                (SELECT COUNT(*) FROM ed_visits) AS totalEdvisits,
+                (SELECT COUNT(*) FROM discharges) AS totalDischarges;
         `;
         
         db.query(query, (err, results) => {
@@ -81,8 +84,11 @@ db.connect((err) => {
     
             const totalPatients = results[0].totalPatients;
             const totalProviders = results[0].totalProviders;
+            const totalVisits = results[0].totalVisits;
+            const totalEdvisits = results[0].totalEdvisits;
+            const totalDischarges = results[0].totalDischarges;
     
-            res.render('home', { totalPatients: totalPatients, totalProviders: totalProviders });
+            res.render('home', { totalPatients: totalPatients, totalProviders: totalProviders, totalVisits: totalVisits, totalEdvisits: totalEdvisits, totalDischarges: totalDischarges });
         });
     });
     
@@ -145,10 +151,21 @@ db.connect((err) => {
     //Add Patient
     app.post('/patients', isAuthenticated, (req, res) => {
         const { 
-            first_name, last_name, date_of_birth, gender, language } = req.body;
+            first_name, 
+            last_name, 
+            date_of_birth, 
+            gender, 
+            language 
+        } = req.body;
         db.query(
             'INSERT INTO patients (first_name, last_name, date_of_birth, gender, language) VALUES (?, ?, ?, ?, ?)', 
-            [first_name, last_name, date_of_birth, gender, language], (err, result) => {
+            [
+                first_name, 
+                last_name, 
+                date_of_birth, 
+                gender, 
+                language
+            ], (err, result) => {
             if (err) throw err;
             res.redirect('/patients');
         });
@@ -176,11 +193,24 @@ db.connect((err) => {
     // Route to handle updating the patient information
     app.post('/patients/edit/:patient_id', (req, res) => {
         const { patient_id } = req.params;
-        const { first_name, last_name, date_of_birth, gender, language } = req.body;
+        const { 
+            first_name, 
+            last_name, 
+            date_of_birth, 
+            gender, 
+            language 
+        } = req.body;
 
         db.query(
             'UPDATE patients SET first_name = ?, last_name = ?, date_of_birth = ?, gender = ?, language = ? WHERE patient_id = ?',
-            [first_name, last_name, date_of_birth, gender, language, patient_id],
+            [
+                first_name, 
+                last_name, 
+                date_of_birth, 
+                gender, 
+                language, 
+                patient_id
+            ],
             (err, result) => {
                 if (err) throw err;
                 
@@ -219,10 +249,23 @@ db.connect((err) => {
     //Add Provider
     app.post('/providers', isAuthenticated, (req, res) => {
         const { 
-            first_name, last_name, provider_specialty, email_address, phone_number, date_joined } = req.body;
+            first_name, 
+            last_name, 
+            provider_specialty, 
+            email_address, 
+            phone_number, 
+            date_joined 
+        } = req.body;
         db.query(
             'INSERT INTO prviders (first_name, last_name, provider_specialty, email_address, phone_number, date_joined) VALUES (?, ?, ?, ?, ?, ?)', 
-            [first_name, last_name, provider_specialty, email_address, phone_number, date_joined], (err, result) => {
+            [
+                first_name, 
+                last_name, 
+                provider_specialty, 
+                email_address, 
+                phone_number, 
+                date_joined
+            ], (err, result) => {
             if (err) throw err;
             res.redirect('/providers');
         });
@@ -250,11 +293,26 @@ db.connect((err) => {
     // Route to handle updating the proovider information
     app.post('/provider/edit/:provider_id', (req, res) => {
         const { provider_id } = req.params;
-        const { first_name, last_name, provider_specialty, email_address, phone_number, date_joined } = req.body;
+        const { 
+            first_name, 
+            last_name, 
+            provider_specialty, 
+            email_address, 
+            phone_number, 
+            date_joined 
+        } = req.body;
 
         db.query(
             'UPDATE patients SET first_name = ?, last_name = ?, provider_specialty = ?, email_address = ?, phone_number = ?, date_joined = ? WHERE patient_id = ?',
-            [first_name, last_name, provider_specialty, email_address, phone_number, date_joined, provider_id],
+            [
+                first_name, 
+                last_name, 
+                provider_specialty, 
+                email_address, 
+                phone_number, 
+                date_joined, 
+                provider_id
+            ],
             (err, result) => {
                 if (err) throw err;
                 
@@ -276,17 +334,325 @@ db.connect((err) => {
 
     //-----------------------------------------------------------------------------
     // Visits CRUD Operations
-
+    app.get('/visits', isAuthenticated, (req, res) => {
+        db.query('SELECT * FROM visits', (err, results) => {
+            if (err) throw err;
+            res.render('visits/visits', { results: results });
+        });
+    });
+    
+    // Route: Display the Add Visit form
+    app.get('/visits/add', isAuthenticated, (req, res) => {
+        res.render('visits/addVisit');
+    });
+    
+    // Route: Add a new visit
+    app.post('/visits', isAuthenticated, (req, res) => {
+        const {
+            patient_id,
+            provider_id,
+            date_of_visit,
+            date_scheduled,
+            visit_department_id,
+            visit_type,
+            blood_pressure_systolic,
+            blood_pressure_diastolic,
+            pulse,
+            visit_status
+        } = req.body;
+    
+        db.query(
+            'INSERT INTO visits (patient_id, provider_id, date_of_visit, date_scheduled, visit_department_id, visit_type, blood_pressure_systolic, blood_pressure_diastolic, pulse, visit_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            [
+                patient_id,
+                provider_id,
+                date_of_visit,
+                date_scheduled,
+                visit_department_id,
+                visit_type,
+                blood_pressure_systolic,
+                blood_pressure_diastolic,
+                pulse,
+                visit_status
+            ],
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/visits');
+            }
+        );
+    });
+    
+    // Route: Display the Edit Visit form
+    app.get('/visits/edit/:visit_id', isAuthenticated, (req, res) => {
+        const { visit_id } = req.params;
+    
+        db.query('SELECT * FROM visits WHERE visit_id = ?', [visit_id], (err, results) => {
+            if (err) throw err;
+    
+            if (results.length > 0) {
+                res.render('visits/editVisit', { visit: results[0] });
+            } else {
+                res.status(404).send('Visit not found');
+            }
+        });
+    });
+    
+    // Route: Update a visit
+    app.post('/visits/edit/:visit_id', isAuthenticated, (req, res) => {
+        const { visit_id } = req.params;
+        const {
+            patient_id,
+            provider_id,
+            date_of_visit,
+            date_scheduled,
+            visit_department_id,
+            visit_type,
+            blood_pressure_systolic,
+            blood_pressure_diastolic,
+            pulse,
+            visit_status
+        } = req.body;
+    
+        db.query(
+            'UPDATE visits SET patient_id = ?, provider_id = ?, date_of_visit = ?, date_scheduled = ?, visit_department_id = ?, visit_type = ?, blood_pressure_systolic = ?, blood_pressure_diastolic = ?, pulse = ?, visit_status = ? WHERE visit_id = ?',
+            [
+                patient_id,
+                provider_id,
+                date_of_visit,
+                date_scheduled,
+                visit_department_id,
+                visit_type,
+                blood_pressure_systolic,
+                blood_pressure_diastolic,
+                pulse,
+                visit_status,
+                visit_id
+            ],
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/visits');
+            }
+        );
+    });
+    
+    // Route: Delete a visit
+    app.post('/visits/delete/:visit_id', isAuthenticated, (req, res) => {
+        const { visit_id } = req.params;
+    
+        db.query('DELETE FROM visits WHERE visit_id = ?', [visit_id], (err, result) => {
+            if (err) throw err;
+            res.redirect('/visits');
+        });
+    });
     //-----------------------------------------------------------------------------
     //Edvisits  CRUD Operations
+    // Get all ED visits
+
+    app.get('/edvisits', isAuthenticated, (req, res) => {
+        db.query(
+            'SELECT * FROM ed_visits', 
+            (err, results) => {
+                if (err) throw err;
+                res.render('edvisits/edvisits', { results: results });
+            });
+    });
+
+    // Add ED Visit Form
+    app.get('/edvisits/add', (req, res) => {
+        res.render('edvisits/addVisit');
+    });
+
+    // Add ED Visit
+    app.post('/edvisits', isAuthenticated, (req, res) => {
+        const { 
+            visit_id, 
+            patient_id, 
+            acuity, 
+            reason_for_visit, 
+            ed_disposition 
+        } = req.body;
+
+        db.query(
+            'INSERT INTO ed_visits (visit_id, patient_id, acuity, reason_for_visit, ed_disposition) VALUES (?, ?, ?, ?, ?)', 
+            [
+                visit_id, 
+                patient_id, 
+                acuity, 
+                reason_for_visit, 
+                ed_disposition
+            ], 
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/edvisits');
+            });
+    });
+
+    // Edit ED Visit Form
+    app.get('/edvisits/edit/:ed_visit_id', (req, res) => {
+        const { ed_visit_id } = req.params;
+
+        db.query(
+            'SELECT * FROM ed_visits WHERE ed_visit_id = ?', 
+            [ed_visit_id], 
+            (err, results) => {
+                if (err) throw err;
+
+                if (results.length > 0) {
+                    res.render('edvisits/editVisit', { edvisit: results[0] });
+                } else {
+                    res.status(404).send("ED Visit not found");
+                }
+            });
+    });
+
+    // Update ED Visit Information
+    app.post('/edvisits/edit/:ed_visit_id', (req, res) => {
+        const { ed_visit_id } = req.params;
+        const { 
+            visit_id, 
+            patient_id, 
+            acuity, 
+            reason_for_visit, 
+            ed_disposition 
+        } = req.body;
+
+        db.query(
+            'UPDATE ed_visits SET visit_id = ?, patient_id = ?, acuity = ?, reason_for_visit = ?, ed_disposition = ? WHERE ed_visit_id = ?',
+            [
+                visit_id, 
+                patient_id, 
+                acuity, 
+                reason_for_visit, 
+                ed_disposition, 
+                ed_visit_id
+            ],
+
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/edvisits');
+            });
+    });
+
+    // Delete ED Visit
+    app.post('/edvisits/delete/:ed_visit_id', (req, res) => {
+        const { ed_visit_id } = req.params;
+
+        db.query(
+            'DELETE FROM ed_visits WHERE ed_visit_id = ?', 
+            [ed_visit_id], 
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/edvisits');
+            });
+    });
+
 
     //-----------------------------------------------------------------------------
     // Discharges CRUD Operations
+    // Get all Discharges
+
+    app.get('/discharges', isAuthenticated, (req, res) => {
+        db.query(
+            'SELECT * FROM discharges', 
+            (err, results) => {
+                if (err) throw err;
+                res.render('discharges/discharges', { results: results });
+            });
+    });
+
+    // Add Discharge Form
+    app.get('/discharges/add', (req, res) => {
+        res.render('discharges/addDischarge');
+    });
+
+    // Add Discharge
+    app.post('/discharges', isAuthenticated, (req, res) => {
+        const { 
+            admission_id, 
+            patient_id, 
+            discharge_date, 
+            discharge_disposition 
+        } = req.body;
+
+        db.query(
+            'INSERT INTO discharges (admission_id, patient_id, discharge_date, discharge_disposition) VALUES (?, ?, ?, ?)', 
+            [
+                admission_id, 
+                patient_id, 
+                discharge_date, 
+                discharge_disposition
+            ], 
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/discharges');
+            });
+    });
+
+    // Edit Discharge Form
+    app.get('/discharges/edit/:discharges_id', (req, res) => {
+        const { discharges_id } = req.params;
+
+        db.query(
+            'SELECT * FROM discharges WHERE discharges_id = ?', 
+            [discharges_id], 
+            (err, results) => {
+                if (err) throw err;
+
+                if (results.length > 0) {
+                    res.render('discharges/editDischarge', { discharge: results[0] });
+                } else {
+                    res.status(404).send("Discharge record not found");
+                }
+            });
+    });
+
+    // Update Discharge Information
+    app.post('/discharges/edit/:discharges_id', (req, res) => {
+        const { discharges_id } = req.params;
+        const { 
+            admission_id, 
+            patient_id, 
+            discharge_date, 
+            discharge_disposition 
+        } = req.body;
+
+        db.query(
+            'UPDATE discharges SET admission_id = ?, patient_id = ?, discharge_date = ?, discharge_disposition = ? WHERE discharges_id = ?',
+            [
+                admission_id, 
+                patient_id, 
+                discharge_date, 
+                discharge_disposition, 
+                discharges_id
+            ],
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/discharges');
+            });
+    });
+
+    // Delete Discharge
+    app.post('/discharges/delete/:discharges_id', (req, res) => {
+        const { discharges_id } = req.params;
+
+        db.query(
+            'DELETE FROM discharges WHERE discharges_id = ?', 
+            [discharges_id], 
+            (err, result) => {
+                if (err) throw err;
+                res.redirect('/discharges');
+            });
+    });
+
+
+    // Loging out logic
     
     app.get('/logout', (req, res) => {
         req.session.destroy();
         res.redirect('/');
     });
+
+    //Server logic
 
     app.listen(process.env.PORT, () => {
         console.log(`Server listening on port ${process.env.PORT}`);
